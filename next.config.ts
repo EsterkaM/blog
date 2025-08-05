07c186b77520273404aff457f175e6c560552c9f
Milan Gallas <gallas.milan@gmail.com>
2025-08-05 19:41:57 +0300

    feat: final domain
    Signed-off-by: Milan Gallas <gallas.milan@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-export const basePath = isProd ? '/blog' : '';
+export const basePath = '';
 
 const nextConfig: NextConfig = {
-  basePath: basePath,
   env: {
     basePath
   },
   output: 'export',
   images: {
-    loader: 'custom',
-    loaderFile: './image-loader.ts'
+    unoptimized: true
   },
   reactStrictMode: true,
 };
