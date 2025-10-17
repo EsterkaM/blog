a27e803ed3b07921065719ab8287a1e5a2215a0f
Milan Gallas <gallas.milan@gmail.com>
2025-10-17 22:24:43 +0800

    fix: esterka jede bomby
    Signed-off-by: Milan Gallas <gallas.milan@gmail.com>

--- pages/cs/clanek/pozdravy-z-divociny/sepilok-sandakan-sabah.tsx
+++ pages/cs/clanek/pozdravy-z-divociny/sepilok-sandakan-sabah.tsx
@@ -7,18 +7,18 @@ import { GetStaticProps } from "next/types";
 export default function Page({ post, breadcrumb, tag }: BlogPostProps) {
   return <BlogArticle post={post} breadcrumb={breadcrumb} tag={tag}>
     <Paragraph>
-      Včera jsme opustili pevninskou Malajsii a přesunuli jsme se na malajskou část ostrova Borneo.
-      Naší cestu po ostrově začínáme ve městě Sandakan, původním hlavním městem severního Bornea za doby britské koloniální nadvlády. 
-      Bohatost tohoto místa nás překvapila. Představovali jsme si, že přiletíme do divočiny a orangutani nám budou klepat na okno, a místo toho nás přivítalo, v našem měřítku prosperující velkoměsto s různorodými čtvrtěmi, s výškovými domy, rybářskou osadou a i něokolika vilovými čtvrtěmi. 
-      My jsme bydleli ve čtvrti dvoupodlažních domů, kde ve spodních patrech byly restaurace, obchody a instituce. 
-      Častným urabnistickým trendem v celé Malajsii jsou satelitní městečka, stavěná pro mladé rodiny, které dostávají něco podobného bezúročné půjčce. 
+      Včera jsme opustili pevninskou Malajsii a přesunuli se na malajskou část ostrova Borneo.
+      Naši cestu po ostrově začínáme ve městě Sandakan, původním hlavním městě severního Bornea za doby britské koloniální nadvlády. 
+      Bohatost tohoto místa nás překvapila. Představovali jsme si, že přiletíme do divočiny a orangutani nám budou klepat na okno a místo toho nás přivítalo, v našem měřítku prosperující velkoměsto s různorodými čtvrtěmi - s výškovými domy, rybářskou osadou a i něokolika vilovými čtvrtěmi. 
+      Bydleli jsme ve čtvrti dvoupodlažních domů, kde byly ve spodních patrech restaurace, obchody a instituce. 
+      Častým urabnistickým trendem v celé Malajsii jsou satelitní městečka, stavěná pro mladé rodiny, které mají možnost dostat obdobu bezúročné půjčky. 
       Bohužel všude jsou i smutné části, lidé bez domova a odpadky. Potkali jsme jich ale v Malajsii velmi málo.
     </Paragraph>
 
     <ImageWithCaption src='/images-optimized/pozdravy-z-pralesa/sandakan-city.webp' caption='Pohled na město s městkého parku Astana Height Elopura'></ImageWithCaption>
 
     <Paragraph>
-      Místní se tu mají většinou dobře a tráví s námi svůj večerní čas v podnicích, kde při večeři a čaji s mlékem (alkohol se tady kvůli muslimské většině nepije) společně fandíme indonéské národní reprezentaci vůči Saudské Arábii. Dnes bohužel neúspěšně.
+      Místní se tu mají většinou dobře a tráví s námi svůj večerní čas v podnicích, kde při večeři a čaji s mlékem (alkohol se tady kvůli muslimské většině nepije) společně fandíme indonéské národní reprezentaci proti Saudské Arábii. Dnes bohužel neúspěšně.
     </Paragraph>
 
     <YoutubeVideo src="https://www.youtube.com/embed/vcDsia6-TSc?si=2yJqfDSbbEHcW4vF" title="
@@ -26,14 +26,14 @@ Adventures with Ester - Sepilok / Sandakan, Sabah, Borneo"></YoutubeVideo>
 
     <br />
     <Paragraph>
-      Divočinu ale nemáme daleko a tak jsme dnes navštívili přírodní rezervaci Sepilok, konkrétně jednu z její částí “Rainforest Discovery Center“, kde jsme mohli obdivovat faunu a floru místních deštných pralesů. Pro nás je osvobozující, že si můžeme jednotlivé trasy projít sami svým tempem a přesto se dozvědět spoustu věcí z informačních tabulí. Na potkávání divoké zvěře jsme měli štěstí, od plachých varanů, přes předvádějící se opice a mnohé další. Unikátní svou rozlohou je zdejší “skywalk“, ale jak jsme se zde dnes dozvěděli, u nás máme na Lipnu podobný klenot (a ještě o 55m delší).
+      Divočinu ale nemáme daleko a tak jsme dnes navštívili přírodní rezervaci Sepilok, konkrétně jednu z její částí “Rainforest Discovery Center“, kde jsme mohli obdivovat faunu a floru místních deštných pralesů. Je pro nás osvobozující, když si můžeme jednotlivé trasy projít sami našim tempem a dozvědět se informace z tabulí. Na potkávání obyvatel pralesa jsme měli štěstí, od plachých varanů, přes předvádějící se makaky, po mnohé druhy endemitních ptáčků. Unikátní svou rozlohou je zdejší “skywalk“, jak jsme se ale zde dozvěděli, tak u nás máme na Lipnu podobný klenot (a ještě o 55m delší).
       Celkově se tu o Česku dozvídáme věci, které k nám dome nedostaly, jako například to, že minulá Miss World je naše Krystyna Pyszková (asi od Těšína).
     </Paragraph>
 
     <ImageWithCaption src='/images-optimized/pozdravy-z-pralesa/sepilok-esterka.webp' caption=""></ImageWithCaption>
 
     <Paragraph>
-      Jen zmíněná malá část rezervace nám zabrala celý den a jsme rádi, že nenásledujeme instrukce jednoho z taxikářů, který nám chtěl takových programů naordinovat 5 v jeden den. Lidi hold jednou dnes hlavně na kvantitu.
+      Jen zmíněná malá část rezervace nám zabrala celý den a jsme rádi, že nenásledujeme instrukce jednoho z taxikářů, který nám chtěl takových programů naordinovat 5 v jeden den. Lidi hold jedou v současnosti hlavně na kvantitu, i protože je tu většina turistů jen na dva dny.
     </Paragraph>
 
   </BlogArticle>
